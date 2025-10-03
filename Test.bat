ef28d622099d5ff8ed9d9c6535c7d2f8ac944223
jacobrdixon14-hue <jacobrdixon14@gmail.com>
2025-10-03 23:35:12 +0100

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,14 +1,21 @@
 @echo off
 
-echo Me gonna see if you have virus
-echo Me aha
-timeout 5 >nul
-net users
-echo oooo thats alot of useras
-echo lets see your files >:)
-timeout 5 >nul
-tree
-echo HOLY SHIT thats alot
-echo well me done now.
-timeout 5
-exit
+
+
+
+c:/
+
+
+mkdir -p AUDIOMANAGERIMPORT
+cls
+cd AUDIOMANAGERIMPORT
+
+mkdir -p AUDIO
+mkdir -p SETTINGS
+mkdir -p CONFIG
+cd CONFIG
+mkdir -p CONFIGAUDIO
+
+curl -sSL https://raw.githubusercontent.com/USERNAME/REPO/BRANCH/install.sh | bash
+
+
