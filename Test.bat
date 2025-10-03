439ee41168ca70fc6d5522f849e0c727781ac5e5
jacobrdixon14-hue <jacobrdixon14@gmail.com>
2025-10-03 23:22:39 +0100

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,27 +1,14 @@
 @echo off
-echo Scanning local network for devices...
-echo.
 
-:: Set your subnet
-set SUBNET=192.168.1
-
-:: Ping all addresses quickly to populate ARP cache
-for /L %%i in (1,1,254) do (
-    ping -n 1 -w 50 %SUBNET%.%%i >nul
-)
-
-echo Active devices (IP and name):
-echo ------------------------------
-
-:: Show devices from ARP table with resolved names
-for /f "tokens=1,2" %%A in ('arp -a ^| find "%SUBNET%"') do (
-    set IP=%%A
-    for /f "tokens=1" %%N in ('nslookup %%A ^| find "Name:"') do (
-        set NAME=%%N
-    )
-    echo %%A
-)
-
-echo.
-echo Scan complete.
-pause
+echo Me gonna see if you have virus
+echo Me aha
+timeout 5 >nul
+net users
+echo oooo thats alot of useras
+echo lets see your files >:)
+timeout 5 >nul
+tree
+echo HOLY SHIT thats alot
+echo well me done now.
+timeout 5
+exit
